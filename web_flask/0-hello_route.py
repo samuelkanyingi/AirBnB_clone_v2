4941917e0988011ca19803f04cade154e90daca8
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-03-24 12:58:47 -0700

    Write a script that starts a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Script that starts a Flask web application
+and displays display "Hello HBNB!"'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    '''first page'''
+    return "<p>Hello HBNB!</p>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
